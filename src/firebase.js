bdd15a6fa74b4e07407e506b4eacd82df5e79cca
Dilhan <kanchanasiriwardhana1@gmail.com>
2023-11-11 13:24:21 +0530

    amazone navbar created

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAd9dQ3WrQ9ppiv-PV-PDZLldLXb4mg2GY",
+  authDomain: "e-challenge-ec51a.firebaseapp.com",
+  projectId: "e-challenge-ec51a",
+  storageBucket: "e-challenge-ec51a.appspot.com",
+  messagingSenderId: "984382205097",
+  appId: "1:984382205097:web:f0104db2a4fae83e0d0db8",
+};
